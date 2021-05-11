461996c15fb2f52bed746414cb055bd3e53f0673
HyeonJeong <yoonhj97@naver.com>
2021-05-11 16:33:00 +0900

    [ADD] 상품 API 기능 완료

--- src/main/java/com/example/study/model/entity/Item.java
+++ src/main/java/com/example/study/model/entity/Item.java
@@ -9,6 +9,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -34,13 +35,13 @@ public class Item {
 
     private String content;
 
-    private Integer price;
+    private BigDecimal price;
 
     private String brandName;
 
     private LocalDateTime registeredAt;
 
-    private String unregisteredAt;
+    private LocalDateTime unregisteredAt;
 
     @CreatedDate
     private LocalDateTime createdAt;
