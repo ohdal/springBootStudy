f73d3e89f431b540d618b3c9baa835c70bfaae2a
HyeonJeong <yoonhj97@naver.com>
2021-05-17 18:33:09 +0900

    [ADD] AdminUser Api CRUD

---
+++ src/main/java/com/example/study/controller/api/AdminUserApiController.java
@@ -0,0 +1,23 @@
+package com.example.study.controller.api;
+
+import com.example.study.model.network.request.AdminUserApiRequest;
+import com.example.study.model.network.response.AdminUserApiResponse;
+import com.example.study.service.AdminUserApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequestMapping("/api/adminUser")
+public class AdminUserApiController extends CrudController<AdminUserApiRequest, AdminUserApiResponse>{
+
+    @Autowired
+    private AdminUserApiLogicService adminUserApiLogicService;
+
+    @PostConstruct
+    public void init() {
+        this.baseService = adminUserApiLogicService;
+    }
+}
